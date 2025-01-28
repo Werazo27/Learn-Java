6137e32bc9057ff2f852cb951e921657afe123fd
Werazo27 <jlpriest27@gmail.com>
2025-01-28 02:23:08 -0500

    Creating Methods

--- src/Main.java
+++ src/Main.java
@@ -14,9 +14,19 @@ public class Main {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         int greetLength = greeting.length();
-        int total = greetLength + number;
+        int total = add(greetLength, number);
+        double ratio = ratio(greetLength, number);
 
         System.out.println(total);
+        System.out.println(ratio);
 
+    }
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static double ratio(int a, int b) {
+        if (a + b == 0) return 0;
+        return (double) a / (a + b) * 100;
     }
 }
